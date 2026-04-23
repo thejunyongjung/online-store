c363d0978c9dade51a6751eec129091ba5de4df8
Junyong Jung <thejunyongjung@gmail.com>
2026-04-23 14:19:14 -0700

    Saving online-store

---
+++ src/main/java/com/pluralsight/Product.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+public class Product {
+    private String sku;
+    private String name;
+    private double price;
+    private String department;
+
+    // Constructor
+    Product(String sku, String name, double price, String department) {
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+        this.department = department;
+    }
+
+    // Getters
+    public String getSku() { return sku;}
+    public String getName() { return name;}
+    public double getPrice() { return price;}
+    public String getDepartment() { return department;}
+
+    // Setters
+    public void setSku(String sku) { this.sku = sku; }
+    public void setName(String name) { this.name = name; }
+    public void setPrice(double price) { this.price = price; }
+    public void setDepartment(String department) { this.department = department; }
+
+    // toString Method
+    @Override
+    public String toString() {
+        return String.format("%-10s %-30s $%8.2f %s", sku, name, price, department);
+    }
+}
